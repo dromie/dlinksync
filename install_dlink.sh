c211ec106f47a0dbf18a7aa4aabb6de874ebc8a1
dromie <dromie@gmail.com>
2014-10-24 16:07:14 +0200

    installer_added

---
+++ install_dlink.sh
@@ -0,0 +1,21 @@
+#!/ffp/bin/bash
+
+if [ ! -d "$1" ];then
+	TMPDIR=`mktemp -d -t`
+	pushd $TMPDIR
+	curl https://codeload.github.com/dromie/dlinksync/zip/master >master.zip
+	unzip master.zip
+	if [ -d dlinksync-master ];then
+		dlinksync-master/`basename $0` $PWD
+	fi
+	popd
+else
+	pushd "$1"
+	DEST=ffp/opt/dlinksync
+	mkdir -p $DEST
+	cp * $DEST/
+	ln -sf $DEST/manual_sync.cgi /var/www/cgi-bin/manual_sync.cgi
+	ln -sf $DEST/manual_sync.html /var/www/manual_sync.html
+	popd
+fi
+
